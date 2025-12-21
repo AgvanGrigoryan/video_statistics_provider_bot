f3d0b8d392b59a0e2efe2c9aae9bd9d7decda80a
Agvan <101641443+AgvanGrigoryan@users.noreply.github.com>
2025-12-22 00:17:27 +0400

    created indexes for tables and some minor changes

--- database/models.py
+++ database/models.py
@@ -1,10 +1,14 @@
 from datetime import datetime
+from uuid import UUID
 
 from sqlalchemy import (
     DateTime,
     ForeignKey,
+    Index,
     Integer,
+    String,
 )
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.base import Base
@@ -19,11 +23,17 @@ class VideoModel(Base, TimestampMixin, MetricsCountMixin):
     """
     __tablename__ = "videos"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
-    creator_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    id: Mapped[UUID] = mapped_column(PG_UUID(as_uuid=True), primary_key=True, nullable=False)
+    creator_id: Mapped[str] = mapped_column(String, nullable=False)
     video_created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
-        nullable=False
+        nullable=False,
+    )
+
+    __table_args__ = (
+        Index(None, "creator_id"),
+        Index(None, "video_created_at"),
+        Index(None, "views_count"),
     )
 
  
@@ -36,14 +46,20 @@ class VideoSnapshotModel(Base, TimestampMixin, MetricsCountMixin):
 
     __tablename__ = "video_snapshots"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
-    video_id: Mapped[int] = mapped_column(
+    id: Mapped[str] = mapped_column(String, primary_key=True, nullable=False)
+    video_id: Mapped[UUID] = mapped_column(
+        PG_UUID(as_uuid=True),
         ForeignKey("videos.id", ondelete="CASCADE"),
         nullable=False,
-        index=True
     )
     
     delta_views_count: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
     delta_likes_count: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
     delta_comments_count: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
     delta_reports_count: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+
+    __table_args__ = (
+        Index(None, "created_at"),
+        Index(None, "video_id"),
+        Index(None, "delta_views_count"),
+    )
